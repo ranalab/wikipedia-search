fa658fa39df4d8c6603c1cd2237355e7f8944fe8
ranalab <webtowhid@gmail.com>
2024-06-29 12:55:23 +0600

    W-S version-1.0

---
+++ js/app.js
@@ -0,0 +1,77 @@
+const searchTermElem = document.querySelector('#searchTerm');
+const searchResultElem = document.querySelector('#searchResult');
+
+searchTermElem.focus();
+
+searchTermElem.addEventListener('input', function (event) {
+    event.target.value ? search(event.target.value) : false;
+});
+
+
+const debounce = (fn, delay = 500) => {
+    let timeoutId;
+
+    return (...args) => {
+        // cancel the previous timer
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+
+        // setup a new timer
+        timeoutId = setTimeout(() => {
+            fn.apply(null, args);
+        }, delay);
+    };
+};
+
+
+const search = debounce(async (searchTerm) => {
+    // if the search term is removed.
+    // reset the search result
+    if (!searchTerm) {
+        // reset the search result
+        searchResultElem.innerHTML = '';
+        return;
+    }
+
+
+    try {
+        // make an API request
+        const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchTerm}`;
+        const response = await fetch(url);
+        const searchResults = await response.json();
+
+        // render search result
+        const searchResultHtml = generateSearchResultHTML(searchResults.query.search, searchTerm);
+
+        // add the search result to  the searchResultElem
+        searchResultElem.innerHTML = searchResultHtml;
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+const stripHTML = (html) => {
+    let div = document.createElement('div');
+    div.innerHTML = html;
+    return div.textContent || div.innerText;
+}
+
+const highlight = (str, keyword, className = "highlight") => {
+    const h1 = `<span class="${className}">${keyword}</span>`;
+    return str.replace(new RegExp(keyword, 'gi'), h1);
+};
+
+const generateSearchResultHTML = (results, searchTerm) => {
+    return results.map((result) => {
+        const title = highlight(stripHTML(result.title), searchTerm);
+        const snippet = highlight(stripHTML(result.snippet), searchTerm);
+
+        return `<article>
+            <a href="https://en.wikipedia.org/?curid=${result.pageid}">${title}</a>
+            <div class="summary">${snippet}...</div>
+        </article>`
+    }).join('');
+}
+
